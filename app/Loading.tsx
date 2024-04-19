b8b462541be8d5bf29ca72c26a2c5025378436af
DevendraBhoraniya <bhoraniyadevendra@gmail.com>
2024-04-20 01:35:44 +0530

    Finla : Project Complete

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Loader from "./components/Loader";
 
 const Loading = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <div>
+      <Loader />
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
